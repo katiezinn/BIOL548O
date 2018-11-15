73762214e0462a936521ccd47b77612753387ed9
Katie Zinn <katiezinn@gmail.com>
2018-11-15 15:10:06 -0800

    notes

---
+++ Notes/Tiday_data_tips.R
@@ -0,0 +1,12 @@
+Tidy data :)
+
+1) select (df, col1, col2)
+  get columns
+2) filter(df, height >= 190cm)
+  get rows
+3) mutate(df, rate=cases/population)
+
+gather --> make longer
+spread --> make wider
+
+this is a useful link for wrangling data https://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
