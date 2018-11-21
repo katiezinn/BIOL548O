009986a9ca4936e3b651d19d0d7a9edd7399f62e
Katie Zinn <katiezinn@gmail.com>
2018-11-20 16:07:05 -0800

    November 20th class update

--- Tiday_data_tips.R
+++ Tiday_data_tips.R
@@ -2,22 +2,11 @@ Tidy data :)
 
 1) select (df, col1, col2)
   get columns
-  
 2) filter(df, height >= 190cm)
   get rows
-  
 3) mutate(df, rate=cases/population)
-
-4) group_by(df, cases/pop)
--attach a label to all the things you group by
--summariza afterwards will take the mean of whatever things are have group_by tags
-
-5) summarize(df, avg = mean(cases))
 
 gather --> make longer
 spread --> make wider
 
 this is a useful link for wrangling data https://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
-
-PIPING %>%
-  
---
+++ Untitled.R
@@ -0,0 +1,10 @@
+Tidy data :)
+
+1) select (df, col1, col2)
+  get columns
+2) filter(df, height >= 190cm)
+  get rows
+3) mutate(df, rate=cases/population)
+
+gather --> make longer
+spread --> make wider
